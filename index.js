04e050a69eabd94717541519973ab41a780d3b98
Husnain Khan <145112113+Husnain467@users.noreply.github.com>
2023-12-20 12:07:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(cors(
-    {
-        source:"*",
-        methods:["GET","POST","PUT","UPDATE","DELETE"],
-    }
-
-));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "http://localhost:3000", // Update to your frontend URL
+    methods: ["POST", "GET","PUT","DELETE"],
+    credentials: true
+  }));
 
 mongoose.connect('mongodb+srv://husnaink467:5262@cluster0.ghk3bf3.mongodb.net/?retryWrites=true&w=majority');
 
